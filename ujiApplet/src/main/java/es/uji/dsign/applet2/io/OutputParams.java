e2fe7003cbc64630e128c527359dcbdaf9f32501
Paul Santapau <santapau@uji.es>
2008-03-07 08:21:18 +0000

    git-svn-id: http://jira.uji.es/repos/uji/tags/CryptoApplet_2.0.1@2183 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ ujiApplet/src/main/java/es/uji/dsign/applet2/io/OutputParams.java
@@ -0,0 +1,16 @@
+package es.uji.dsign.applet2.io;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.swing.JApplet;
+
+import es.uji.dsign.applet2.SignatureApplet;
+
+public interface OutputParams
+{
+	public void setSignFormat(byte[] signFormat) throws IOException;
+	public void setSignData(byte[] data) throws IOException;
+	public void signOk();
+	public void flush();
+}
