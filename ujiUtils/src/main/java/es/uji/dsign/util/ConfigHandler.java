6b7762867f291a5dcd959fd303c8440b54d31b60
Paul Santapau <santapau@uji.es>
2008-03-07 08:22:03 +0000

    git-svn-id: http://jira.uji.es/repos/uji/tags/CryptoApplet_2.0.1@2695 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ ujiUtils/src/main/java/es/uji/dsign/util/ConfigHandler.java
@@ -0,0 +1,95 @@
+package es.uji.dsign.util;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ConfigHandler {
+
+	private static ConfigHandler config;
+	private static Properties prop;
+	private static String configFile= "jar://ujiCrypto.conf";
+	
+	private ConfigHandler()
+	{
+		try
+		{
+			InputStream is;
+			prop = new Properties();
+		
+			if(configFile.startsWith("jar://")) {
+            	ClassLoader cl = ConfigHandler.class.getClassLoader();
+                is = cl.getResourceAsStream(configFile.substring(6));
+            } else {
+                is = new FileInputStream(configFile);
+            }
+		
+			prop.load(is);	
+			
+			setConstantProperties();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			prop=null;
+		}
+	}
+	
+	private void setConstantProperties() {
+	 	/* Here we must set some constant properties */
+      	prop.put("DIGIDOC_NOTARY_IMPL",
+      			"es.uji.dsign.crypto.digidoc.factory.BouncyCastleNotaryFactory");
+      	prop.put("DIGIDOC_FACTORY_IMPL", 
+      			    "es.uji.dsign.crypto.digidoc.factory.SAXDigiDocFactory");
+     	prop.put("DIGIDOC_TIMESTAMP_IMPL", 
+     			    "es.uji.dsign.crypto.digidoc.factory.BouncyCastleSignatureTimestampFactory");
+        prop.put("CANONICALIZATION_FACTORY_IMPL", 
+        		    "es.uji.dsign.crypto.digidoc.factory.DOMCanonicalizationFactory");
+      	prop.put("CRL_FACTORY_IMPL", 
+      			    "es.uji.dsign.crypto.digidoc.factory.CRLCheckerFactory");
+		prop.put("DIGIDOC_SECURITY_PROVIDER", 
+				    "org.bouncycastle.jce.provider.BouncyCastleProvider");
+      	prop.put("DIGIDOC_SECURITY_PROVIDER_NAME",
+      			    "BC");
+    	prop.put("DIGIDOC_VERIFY_ALGORITHM",
+			"RSA//");
+      	/* */
+	}
+
+	public static void  setConfigFile(String file){
+		configFile=file;
+	}
+	
+	public static String getProperty(String key){
+		
+		if ( config==null )
+			config= new ConfigHandler();
+		
+		return prop.getProperty(key);
+		
+	}
+	
+	public static Properties getProperties(){
+		
+		if ( config==null )
+			config= new ConfigHandler();
+		
+		return prop;
+		
+	}
+	
+	public int getIntProperty(String key, int def){
+	
+		if ( config==null )
+			config= new ConfigHandler();
+		
+		if (prop.getProperty(key)!=null)
+			return Integer.parseInt(prop.getProperty(key));
+		
+		return def;
+		
+	}
+	
+}
---
+++ ujiUtils/src/main/java/es/uji/dsign/util/net/SocketDataInputStreamReader.java
@@ -0,0 +1,44 @@
+package es.uji.dsign.util.net;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class SocketDataInputStreamReader
+{
+	private DataInputStream _dis;
+
+	public SocketDataInputStreamReader(DataInputStream dis)
+	{
+		_dis = dis;
+	}
+
+	public byte readByte() throws IOException
+	{
+		return _dis.readByte();
+	}
+
+	public int readInt() throws IOException
+	{
+		return Integer.reverseBytes(_dis.readInt());
+	}
+
+	public byte[] readByteArray(int size) throws IOException
+	{
+		int read = 0, err = 0;
+		byte[] res = new byte[size];
+
+		while (read < size)
+		{
+			err = _dis.read(res, read, size - read);
+			if (err == -1)
+			{
+				throw new IOException("ErrorReadingFromSocket");
+			}
+			else
+			{
+				read += err;
+			}
+		}
+		return res;
+	}
+}
