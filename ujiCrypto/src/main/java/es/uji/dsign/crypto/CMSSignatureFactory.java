b166c5ccfa32ba95e22541bd1303bd5b86636c29
Paul Santapau <santapau@uji.es>
2008-06-12 10:33:19 +0000

    Added the timestamp feature. Now if in configuration file we put
      CMS_TIMESTAMPING=true
      CMS_TSA_URL=tsa_url 
      
    We will get an structure as follows: 
     <data> 
       <cms_signature>
         base64 encoded pkcs#7 signature
       </cms_signature>
       <cms_timestamp>
         base64 encoded pkcs#7 timestamp
       </cms_timestamp>
     </data>
     
    where base64 encoded pkcs#7 timestamp is 
     sha1(b64decode(base64 encoded pkcs#7 signature))
    signed by the TSA.
    
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2739 e6edf6fb-f266-4316-afb4-e53d95876a76

--- ujiCrypto/src/main/java/es/uji/dsign/crypto/CMSSignatureFactory.java
+++ ujiCrypto/src/main/java/es/uji/dsign/crypto/CMSSignatureFactory.java
@@ -1,14 +1,16 @@
 package es.uji.dsign.crypto;
 
 import java.security.KeyStoreException;
+import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.Provider;
-import java.security.cert.CertStore;
+import java.security.cert. CertStore;
 import java.security.cert.Certificate;
 import java.security.cert.CollectionCertStoreParameters;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.cms.CMSProcessableByteArray;
@@ -16,6 +18,8 @@ import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.CMSSignedGenerator;
 import org.bouncycastle.cms.MyCMSSignedDataGenerator;
 
+import es.uji.dsign.util.Base64;
+import es.uji.dsign.util.ConfigHandler;
 import es.uji.dsign.util.i18n.LabelManager;
 
 
@@ -57,11 +61,53 @@ public class CMSSignatureFactory extends AbstractSignatureFactory implements ISi
 		
 		if (data != null)
 		{
-			return data.getEncoded();
+			
+			// Now we must check if a timestamp must be calculated 
+			// reading the configuration file.
+			
+			Properties prop= ConfigHandler.getProperties();
+			if ( prop == null ){
+				_strerr= LabelManager.get("ERROR_CMS_CONFIG_NOT_FOUND");
+				return null;
+			}
+			
+			String doTs= prop.getProperty("CMS_TIMESTAMPING");
+			String tsaUrl= prop.getProperty("CMS_TSA_URL");
+			
+			if (doTs != null && doTs.toLowerCase().equals("true")){
+
+				MessageDigest dig= MessageDigest.getInstance("SHA1");
+				dig.update(data.getEncoded());
+				byte[] tsHash= dig.digest();
+
+				log.info("The calculated hash for timestamp inclussion is " + new String(Base64.encode(tsHash)));
+
+				if( tsaUrl==null ){
+					_strerr= LabelManager.get("ERROR_CMS_CONFIG_TSAURL_NOT_FOUND");
+					log.info("Bad configuration file: cannot get TSA URL");
+					return null;
+				}
+
+				byte[] ts= TimeStampFactory.getTimeStamp(tsaUrl, tsHash);
+				if (ts== null){
+					_strerr= LabelManager.get("ERROR_CMS_CALCULATING_TS");
+					log.info("Cannot calculate timestamp from: " + tsaUrl);
+					return null;
+				}
+				return ("<data>\r\n  <cms_signature>\r\n" 
+				         + new String(Base64.encode(data.getEncoded(),true)) +
+				       "\r\n  </cms_signature>\r\n  <cms_timestamp>\r\n" 
+				         + new String(Base64.encode(ts,true)) + "\r\n  </cms_timestamp>\r\n</data>\r\n").getBytes();  
+				
+			}
+			else{
+				return Base64.encode(data.getEncoded(),true);  
+			}
 		}
 		else
 		{
 			_strerr= LabelManager.get("ERROR_CMS_SIGNATURE");
+			System.out.println("Fuera por 4");
 			return null;
 		}
 	}
