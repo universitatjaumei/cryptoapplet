86ca176129281903b63dab5edcc32f0ebae554ef
Paul Santapau <santapau@uji.es>
2008-06-12 10:34:36 +0000

    Added the timestamp feature. Now if in configuration file we put
      CMS_TIMESTAMPING=true
      CMS_TSA_URL=tsa_url 
      
    We will get an structure as follows: 
     <data> 
       <cms_signature>
         base64 encoded pkcs#7 signature
       </cms_signature>
       <cms_timestamp>
         base64 encoded pkcs#7 timestamp
       </cms_timestamp>
     </data>
     
    where base64 encoded pkcs#7 timestamp is 
     sha1(b64decode(base64 encoded pkcs#7 signature))
    signed by the TSA.
    
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2231 e6edf6fb-f266-4316-afb4-e53d95876a76

--- ujiCrypto/src/main/java/es/uji/dsign/crypto/CMSHashSignatureFactory.java
+++ ujiCrypto/src/main/java/es/uji/dsign/crypto/CMSHashSignatureFactory.java
@@ -1,6 +1,7 @@
 package es.uji.dsign.crypto;
 
 import java.security.KeyStoreException;
+import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.cert.CertStore;
@@ -9,6 +10,7 @@ import java.security.cert.CollectionCertStoreParameters;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.cms.CMSProcessableByteArray;
@@ -16,58 +18,106 @@ import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.CMSSignedGenerator;
 import org.bouncycastle.cms.MyCMSSignedDataGenerator;
 
+import es.uji.dsign.util.Base64;
+import es.uji.dsign.util.ConfigHandler;
 import es.uji.dsign.util.i18n.LabelManager;
 
 
 public class CMSHashSignatureFactory extends AbstractSignatureFactory implements ISignFormatProvider
 {
-    private Logger log = Logger.getLogger(CMSHashSignatureFactory.class);
-    private String _strerr= "";
-    
+	private Logger log = Logger.getLogger(CMSHashSignatureFactory.class);
+	private String _strerr= "";
+
 	public byte[] formatSignature(byte[] hash, X509Certificate sCer, PrivateKey pk, Provider pv) throws KeyStoreException, Exception
 	{
 		// Init the provider registry
 		super.initProviderList();
-		
+
 		MyCMSSignedDataGenerator gen = new MyCMSSignedDataGenerator();
-		
+
 		if ( sCer == null ){
 			_strerr= LabelManager.get("ERROR_CMS_NOCERT");
 			return null;
 		}
-				
+
 		if ( pk == null ){
 			_strerr= LabelManager.get("ERROR_CMS_NOKEY");
 			return null;
 		}
-		
+
 		gen.addSigner(pk, (X509Certificate) sCer, CMSSignedGenerator.DIGEST_SHA1);
 		CMSProcessableByteArray cba = new CMSProcessableByteArray(hash);
 
 		List<Certificate> certList = new ArrayList<Certificate>();
-		
+
 		//TODO:  Add the intermediate CAs if we have them
 		certList.add(sCer);
-		
+
 		CertStore certst = CertStore.getInstance("Collection", new CollectionCertStoreParameters(certList), "BC");
-	
+
 		gen.addCertificatesAndCRLs(certst);
 		gen.setHash(hash);
-	
-			
+
+
 		CMSSignedData data = gen.generate(cba, pv.getName());
-		
+
 		if (data != null)
 		{
-			return data.getEncoded();
+			if (data != null)
+			{
+
+				// Now we must check if a timestamp must be calculated 
+				// reading the configuration file.
+
+				Properties prop= ConfigHandler.getProperties();
+				if ( prop == null ){
+					_strerr= LabelManager.get("ERROR_CMS_CONFIG_NOT_FOUND");
+					log.info("Cannot read config properties");
+					return null;
+				}
+
+				String doTs= prop.getProperty("CMS_TIMESTAMPING");
+				String tsaUrl= prop.getProperty("CMS_TSA_URL");
+
+				if (doTs != null && doTs.toLowerCase().equals("true")){
+
+					if( tsaUrl==null ){
+						_strerr= LabelManager.get("ERROR_CMS_CONFIG_TSAURL_NOT_FOUND");
+						log.info("Bad configuration file: cannot get TSA URL");
+						return null;
+					}
+
+					MessageDigest dig= MessageDigest.getInstance("SHA1");
+					dig.update(data.getEncoded());
+					byte[] tsHash= dig.digest();
+
+					byte[] ts= TimeStampFactory.getTimeStamp(tsaUrl, tsHash);
+
+					if (ts== null){
+						_strerr= LabelManager.get("ERROR_CMS_CALCULATING_TS");
+						log.info("Cannot calculate timestamp from: " + tsaUrl);
+						return null;
+					}
+					
+					return ("<data>\r\n  <cms_signature>\r\n" 
+					         + new String(Base64.encode(data.getEncoded(),true)) +
+					       "\r\n  </cms_signature>\r\n  <cms_timestamp>\r\n" 
+					         + new String(Base64.encode(ts,true)) + "\r\n  </cms_timestamp>\r\n</data>\r\n").getBytes();		
+				}
+				else{
+					return Base64.encode(data.getEncoded(),true);
+				}
+			}	
 		}
 		else
 		{
 			_strerr= LabelManager.get("ERROR_CMS_SIGNATURE");
 			return null;
-		}
+		}	
+		
+		return null;
 	}
-	
+
 	public String getError(){
 		return _strerr;
 	}
