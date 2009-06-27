804fbbac25e0678792ade2de9df553cf4ef9d4f9
Ricardo Borillo <borillo@gmail.com>
2009-06-27 10:31:04 +0000

    Added new verifier class
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2907 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-jxades/src/main/java/es/uji/security/crypto/jxades/FacturaeSignatureVerifier.java
+++ uji-crypto-jxades/src/main/java/es/uji/security/crypto/jxades/FacturaeSignatureVerifier.java
@@ -1,9 +1,51 @@
 package es.uji.security.crypto.jxades;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import net.java.xades.security.xml.SignatureStatus;
+import net.java.xades.security.xml.ValidateResult;
+import net.java.xades.security.xml.XAdES.XAdES;
+import net.java.xades.security.xml.XAdES.XAdES_BES;
+import net.java.xades.security.xml.XAdES.XMLAdvancedSignature;
+
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import es.uji.security.crypto.VerificationDetails;
+
 public class FacturaeSignatureVerifier
 {
-    public static void main(String[] args)
+    public VerificationDetails verify(byte[] signedData) throws ParserConfigurationException,
+            SAXException, IOException
     {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Element element = db.parse(new ByteArrayInputStream(signedData)).getDocumentElement();
 
+        XAdES_BES xades = (XAdES_BES) XAdES.newInstance(XAdES.BES, element);
+
+        XMLAdvancedSignature fileXML = XMLAdvancedSignature.newInstance(xades);
+        List<SignatureStatus> st = fileXML.validate();
+
+        VerificationDetails verificationDetails = new VerificationDetails();
+        verificationDetails.setResult(true);
+
+        for (SignatureStatus status : st)
+        {
+            if (status.getValidateResult() != ValidateResult.VALID)
+            {
+                verificationDetails.setResult(false);
+                verificationDetails.addError("Sign validation error: " + status.getReasonsAsText());
+            }
+        }
+
+        return verificationDetails;
     }
 }
