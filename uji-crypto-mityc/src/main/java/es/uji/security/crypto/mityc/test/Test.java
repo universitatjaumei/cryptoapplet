e926f5907327f527d43952324e463cca4485aec9
Ricardo Borillo <borillo@gmail.com>
2009-07-28 12:29:46 +0000

    Adapted to the new SignatureOptions structure
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2432 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-mityc/src/main/java/es/uji/security/crypto/mityc/test/Test.java
+++ uji-crypto-mityc/src/main/java/es/uji/security/crypto/mityc/test/Test.java
@@ -1,31 +1,24 @@
 package es.uji.security.crypto.mityc.test;
 
 import java.io.FileInputStream;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.PrivateKey;
 import java.security.Security;
-import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-import es.uji.security.crypto.ISignFormatProvider;
-import es.uji.security.crypto.SignatureOptions;
-import es.uji.security.crypto.mityc.MitycXAdESSignatureFactory;
 import es.uji.security.crypto.mityc.MitycXAdESSignatureValidator;
 import es.uji.security.util.OS;
 
-public class Test 
+public class Test
 {
-	public static void main(String[] args) throws Exception 
-	{
+    public static void main(String[] args) throws Exception
+    {
         BouncyCastleProvider bcp = new BouncyCastleProvider();
         Security.addProvider(bcp);
 
-		byte[] signedData = OS.inputStreamToByteArray(new FileInputStream("/tmp/test.xml"));
-				
-		MitycXAdESSignatureValidator v = new MitycXAdESSignatureValidator();		
-		System.out.println((v.verify(signedData)) ? "OK" : "MAL");
-	}
+        byte[] signedData = OS.inputStreamToByteArray(new FileInputStream(
+                "src/main/resources/out.xml"));
+
+        MitycXAdESSignatureValidator v = new MitycXAdESSignatureValidator();
+        System.out.println((v.verify(signedData)) ? "OK" : "MAL");
+    }
 }
