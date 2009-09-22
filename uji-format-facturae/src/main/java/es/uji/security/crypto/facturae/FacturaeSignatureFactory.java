3e90025488969d11cb5498d8d460222311bbec4d
Ricardo Borillo <borillo@gmail.com>
2009-09-22 08:58:08 +0000

    Problem reading streams
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2986 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-format-facturae/src/main/java/es/uji/security/crypto/facturae/FacturaeSignatureFactory.java
+++ uji-format-facturae/src/main/java/es/uji/security/crypto/facturae/FacturaeSignatureFactory.java
@@ -1,5 +1,7 @@
 package es.uji.security.crypto.facturae;
 
+import java.io.ByteArrayInputStream;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -10,17 +12,20 @@ import es.uji.security.crypto.ISignFormatProvider;
 import es.uji.security.crypto.SignatureOptions;
 import es.uji.security.crypto.SignatureResult;
 import es.uji.security.crypto.jxades.JXAdESSignatureFactory;
+import es.uji.security.util.OS;
 
 public class FacturaeSignatureFactory implements ISignFormatProvider
 {
     public SignatureResult formatSignature(SignatureOptions signatureOptions) throws Exception
     {
+        byte[] data = OS.inputStreamToByteArray(signatureOptions.getDataToSign());
+        
         // Check if input data is already signed
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setNamespaceAware(true);
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         
-        Document document  = documentBuilder.parse(signatureOptions.getDataToSign());
+        Document document  = documentBuilder.parse(new ByteArrayInputStream(data));
         
         NodeList result = document.getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#", "Signature");
         int numSignatures = result.getLength();
@@ -38,6 +43,7 @@ public class FacturaeSignatureFactory implements ISignFormatProvider
         signatureOptions.setCoSignEnabled(false);
         signatureOptions.setPolicyIdentifier("http://www.facturae.es/politica_de_firma_formato_facturae/politica_de_firma_formato_facturae_v3_1.pdf");
         signatureOptions.setPolicyDescription("Pol\u00edtica de firma electr\u00f3nica para facturaci\u00f3n electr\u00f3nica con formato Facturae");
+        signatureOptions.setDataToSign(new ByteArrayInputStream(data));
 
         JXAdESSignatureFactory signatureFactory = new JXAdESSignatureFactory();
         
