c8c883ade19649978a24da78ff276379bcf504bb
Ricardo Borillo <borillo@gmail.com>
2009-09-25 08:03:52 +0000

    Change serialization method to avoid xalan dependency
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2987 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-format-facturae/src/main/java/es/uji/security/crypto/facturae/FacturaeSignatureFactory.java
+++ uji-format-facturae/src/main/java/es/uji/security/crypto/facturae/FacturaeSignatureFactory.java
@@ -19,34 +19,37 @@ public class FacturaeSignatureFactory implements ISignFormatProvider
     public SignatureResult formatSignature(SignatureOptions signatureOptions) throws Exception
     {
         byte[] data = OS.inputStreamToByteArray(signatureOptions.getDataToSign());
-        
+
         // Check if input data is already signed
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setNamespaceAware(true);
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        
-        Document document  = documentBuilder.parse(new ByteArrayInputStream(data));
-        
-        NodeList result = document.getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#", "Signature");
+
+        Document document = documentBuilder.parse(new ByteArrayInputStream(data));
+
+        NodeList result = document.getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#",
+                "Signature");
         int numSignatures = result.getLength();
-        
+
         if (numSignatures > 0)
         {
             SignatureResult signatureResult = new SignatureResult();
             signatureResult.setValid(false);
             signatureResult.addError("CoSign for Facturae not supported");
-            
+
             return signatureResult;
         }
-                
+
         // Call jXAdES format signature
         signatureOptions.setCoSignEnabled(false);
-        signatureOptions.setPolicyIdentifier("http://www.facturae.es/politica_de_firma_formato_facturae/politica_de_firma_formato_facturae_v3_1.pdf");
-        signatureOptions.setPolicyDescription("Pol\u00edtica de firma electr\u00f3nica para facturaci\u00f3n electr\u00f3nica con formato Facturae");
+        signatureOptions
+                .setPolicyIdentifier("http://www.facturae.es/politica_de_firma_formato_facturae/politica_de_firma_formato_facturae_v3_1.pdf");
+        signatureOptions
+                .setPolicyDescription("Pol\u00edtica de firma electr\u00f3nica para facturaci\u00f3n electr\u00f3nica con formato Facturae");
         signatureOptions.setDataToSign(new ByteArrayInputStream(data));
 
         JXAdESSignatureFactory signatureFactory = new JXAdESSignatureFactory();
-        
+
         return signatureFactory.formatSignature(signatureOptions);
     }
 }
