8d54c7599cdabaad81baf46d9885f9ad3e19133e
Ricardo Borillo <borillo@gmail.com>
2009-09-25 08:03:52 +0000

    Change serialization method to avoid xalan dependency
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2478 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-format-facturae/src/main/java/es/uji/security/crypto/facturae/test/TestFacturaeFactory.java
@@ -0,0 +1,57 @@
+package es.uji.security.crypto.facturae.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import es.uji.security.crypto.SignatureOptions;
+import es.uji.security.crypto.SignatureResult;
+import es.uji.security.crypto.facturae.FacturaeSignatureFactory;
+import es.uji.security.util.OS;
+
+public class TestFacturaeFactory
+{
+    public static void main(String[] args) throws Exception
+    {
+        BouncyCastleProvider bcp = new BouncyCastleProvider();
+        Security.addProvider(bcp);
+
+        // Cargando certificado de aplicacion
+        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keystore.load(new FileInputStream("../uji.keystore"), "cryptoapplet".toCharArray());
+
+        // Recuperando clave privada para firmar
+        X509Certificate certificate = (X509Certificate) keystore.getCertificate(keystore.aliases()
+                .nextElement());
+        Key key = keystore.getKey(keystore.aliases().nextElement(), "cryptoapplet".toCharArray());
+
+        SignatureOptions signatureOptions = new SignatureOptions();
+        signatureOptions.setDataToSign(new FileInputStream("src/main/resources/in-facturae.xml"));
+        signatureOptions.setCertificate(certificate);
+        signatureOptions.setPrivateKey((PrivateKey) key);
+        signatureOptions.setProvider(bcp);
+
+        FacturaeSignatureFactory facturaeSignatureFactory = new FacturaeSignatureFactory();
+        SignatureResult signatureResult = facturaeSignatureFactory
+                .formatSignature(signatureOptions);
+
+        if (signatureResult.isValid())
+        {
+            OS.dumpToFile(new File("src/main/resources/out-facturae.xml"), signatureResult
+                    .getSignatureData());
+        }
+        else
+        {
+            for (String error : signatureResult.getErrors())
+            {
+                System.out.println(error);
+            }
+        }
+    }
+}
