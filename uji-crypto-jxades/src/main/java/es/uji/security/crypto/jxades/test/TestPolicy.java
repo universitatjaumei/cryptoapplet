3d6c82c7220dc05fcde17546ff5fbaaa76e8d1f9
Ricardo Borillo <borillo@gmail.com>
2010-03-12 09:07:56 +0000

    Tests extracted to new uji-tests project
    git-svn-id: http://jira.uji.es/repos/uji/trunk@2556 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-jxades/src/main/java/es/uji/security/crypto/jxades/test/TestPolicy.java
+++
@@ -1,9 +0,0 @@
-package es.uji.security.crypto.jxades.test;
-
-public class TestPolicy
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Pol\u00edtica de firma electr\u00f3nica para facturaci\u00f3n electr\u00f3nica con formato Facturae");        
-    }
-}
