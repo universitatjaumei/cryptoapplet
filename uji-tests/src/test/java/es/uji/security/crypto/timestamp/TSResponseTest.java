0974ca2bf0a9839c1f483c33005da4f43fe9a317
Paul Santapau <santapau@uji.es>
2010-07-27 11:14:44 +0000

    TSResponse Test
    git-svn-id: http://jira.uji.es/repos/uji/trunk@3127 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-tests/src/test/java/es/uji/security/crypto/timestamp/TSResponseTest.java
+++ uji-tests/src/test/java/es/uji/security/crypto/timestamp/TSResponseTest.java
@@ -13,16 +13,20 @@ public class TSResponseTest {
 		
 		FileInputStream fts1 = new FileInputStream(TSResponseTest.baseDir + "ts_response_1.bin");
 		FileInputStream fts2 = new FileInputStream(TSResponseTest.baseDir + "ts_response_2.bin");
+		FileInputStream fts3 = new FileInputStream(TSResponseTest.baseDir + "ts_response_3.bin");
 		
 		byte[] bts1= new byte[fts1.available()];
 		byte[] bts2= new byte[fts2.available()];
+		byte[] bts3= new byte[fts2.available()];
 		
 		fts1.read(bts1);
 		fts2.read(bts2);
+		fts3.read(bts3);
 		
 		 
 		try{
-			TSResponse ts1= new TSResponse(bts1); 
+			TSResponse ts1= new TSResponse(bts1);
+			System.out.println("ts1 se ha analizado correctamente.");
 		}
 		catch(Exception e){
 			System.out.println("Parsing de ts1 ha fallado: " + e.getMessage());
@@ -30,9 +34,19 @@ public class TSResponseTest {
 
 		try{
 			TSResponse ts2= new TSResponse(bts2);
+			System.out.println("ts2 se ha analizado correctamente.");
 		}
 		catch(Exception e){
 			System.out.println("Parsing de ts2 ha fallado: " + e.getMessage());
 		}
+		
+		try{
+			TSResponse ts3= new TSResponse(bts3); 
+			System.out.println("ts3 se ha analizado correctamente.");
+		}
+		catch(Exception e){
+			System.out.println("Parsing de ts3 ha fallado: " + e.getMessage());
+		}
+
 	}
 }
