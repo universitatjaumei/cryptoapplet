29a3ff718836640689ac101d86f422900b6bdc8a
Ricardo Borillo <borillo@gmail.com>
2010-07-27 11:23:24 +0000

    Test timestamp response modificado
    git-svn-id: http://jira.uji.es/repos/uji/trunk@3130 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-tests/src/test/java/es/uji/security/crypto/timestamp/TSResponseTest.java
+++ uji-tests/src/test/java/es/uji/security/crypto/timestamp/TSResponseTest.java
@@ -1,53 +1,29 @@
 package es.uji.security.crypto.timestamp;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
+import es.uji.security.crypto.config.OS;
 
-public class TSResponseTest {
+public class TSResponseTest
+{
+    public static String baseDir = "src/main/resources/";
 
-	public static String baseDir = "src/main/resources/";
-
-	public static void main(String[] args) throws IOException{
-		
-		FileInputStream fts1 = new FileInputStream(TSResponseTest.baseDir + "ts_response_1.bin");
-		FileInputStream fts2 = new FileInputStream(TSResponseTest.baseDir + "ts_response_2.bin");
-		FileInputStream fts3 = new FileInputStream(TSResponseTest.baseDir + "ts_response_3.bin");
-		
-		byte[] bts1= new byte[fts1.available()];
-		byte[] bts2= new byte[fts2.available()];
-		byte[] bts3= new byte[fts3.available()];
-		
-		fts1.read(bts1);
-		fts2.read(bts2);
-		fts3.read(bts3);
-		
-		 
-		try{
-			TSResponse ts1= new TSResponse(bts1);
-			System.out.println("ts1 se ha analizado correctamente.");
-		}
-		catch(Exception e){
-			System.out.println("Parsing de ts1 ha fallado: " + e.getMessage());
-		}
-
-		try{
-			TSResponse ts2= new TSResponse(bts2);
-			System.out.println("ts2 se ha analizado correctamente.");
-		}
-		catch(Exception e){
-			System.out.println("Parsing de ts2 ha fallado: " + e.getMessage());
-		}
-		
-		try{
-			TSResponse ts3= new TSResponse(bts3); 
-			System.out.println("ts3 se ha analizado correctamente.");
-		}
-		catch(Exception e){
-			System.out.println("Parsing de ts3 ha fallado: " + e.getMessage());
-		}
-
-	}
+    public static void main(String[] args) throws IOException
+    {
+        for (String fileName : new String[] { "ts_response_1.bin", "ts_response_2.bin",
+                "ts_response_3.bin", "bueno.bin" })
+        {
+            try
+            {
+                new TSResponse(OS.inputStreamToByteArray(new FileInputStream(TSResponseTest.baseDir
+                        + fileName)));
+                System.out.println("Response ok");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error parseando respuesta: " + e.getMessage());
+            }
+        }
+    }
 }
