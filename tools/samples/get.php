d7c46bd99862501e29223cc48d817f25551daa91
Ricardo Borillo <borillo@gmail.com>
2012-03-27 13:01:56 +0000

    Moved tools outside trunk
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@6902 e6edf6fb-f266-4316-afb4-e53d95876a76

--- tools/samples/get.php
+++
@@ -1,7 +0,0 @@
-<?
-
-header('Content-type: application/octet-stream');
-header("Content-Disposition: attachment; filename=\"signed_invoice.xsig\"");
-echo file_get_contents("signature.xsig");
-
-?>
--- tools/samples/pdf.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="signpdf.php" enctype="multipart/form-data">
-    Select the PDF to be signed: <br/>
-    <input checked id="pdf1" type="radio" name="pdf" value="/fich/f1.pdf"> <label for="pdf1">Fichero prueba 1 (149Kb) </label><a href="fich/f1.pdf"><img style="border: none;" src="img/pdf.gif" /></a></label><br/>
-    <input disabled id="pdf2" type="radio" name="pdf" value="pdf2"> <label for="pdf2">Fichero prueba 2 (1MB) </label><a href="javascript:alert('Not available yet!');"><img style="border: none;" src="img/pdf.gif" /></a></label><br/>
-    <input disabled id="pdf3" type="radio" name="pdf" value="pdf3"> <label for="pdf3"> Fichero prueba 3 (20MB) </label> <a href="javascript:alert('Not available yet!');"><img style="border: none;" src="img/pdf.gif" /></a><br/>
-</form>
-
