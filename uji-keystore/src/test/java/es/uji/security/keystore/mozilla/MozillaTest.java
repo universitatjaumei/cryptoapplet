25d90bde1c7cc9dff1836c096c6d1ca67ada0722
Ricardo Borillo <borillo@gmail.com>
2012-03-31 18:15:40 +0000

    Unified firefox, clauer and pkcs11 keyStore managers
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@7015 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-keystore/src/test/java/es/uji/security/keystore/mozilla/MozillaTest.java
+++ uji-keystore/src/test/java/es/uji/security/keystore/mozilla/MozillaTest.java
@@ -3,19 +3,21 @@ package es.uji.security.keystore.mozilla;
 import org.junit.Assert;
 import org.junit.Test;
 
+import es.uji.security.keystore.Firefox;
+
 public class MozillaTest
 {
     @Test
     public void shouldFindExistingLibsoftokn3Installed() throws Exception
     {
-        Mozilla mozilla = new Mozilla();
+        Firefox mozilla = new Firefox();
         Assert.assertNotNull(mozilla.getPKCS11Library());
     }
     
     @Test
     public void shouldFindCurrentProfileDirectory() throws Exception
     {
-        Mozilla mozilla = new Mozilla();
+        Firefox mozilla = new Firefox();
         Assert.assertNotNull(mozilla.getCurrentProfileDirectory());
     }    
 }
--- uji-keystore/src/test/java/es/uji/security/keystore/pkcs11/PKCS11KeyStoreTest.java
+++ uji-keystore/src/test/java/es/uji/security/keystore/pkcs11/PKCS11KeyStoreTest.java
@@ -11,31 +11,17 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import es.uji.security.keystore.Firefox;
 import es.uji.security.keystore.SimpleKeyStore;
-import es.uji.security.keystore.mozilla.Mozilla;
 
 public class PKCS11KeyStoreTest
 {
     private static final int NUM_CERTIFICATES_IN_STORE = 2;
     private static final String SIGN_CERTIFICATE_ALIAS = "CIFRADO/c=es,o=generalitat valenciana,ou=pkigva,cn=accv-ca2/8890499274928531747";
 
-    private String getDriverConfiguration()
+    private byte[] getDriverConfiguration()
     {
-        Mozilla mozilla = new Mozilla();
-        String pkcs11FilePath = "/usr/lib/i386-linux-gnu/nss/libsoftokn3.so";
-        String currentprofile = mozilla.getCurrentProfiledir();
-
-        StringBuilder config = new StringBuilder();
-
-        config.append("name=NSS").append("\n");
-        config.append("library=").append(pkcs11FilePath).append("\n");
-        config.append("attributes=compatibility").append("\n");
-        config.append("slot=2").append("\n");
-        config.append("nssArgs=\"configdir='").append(currentprofile).append("' ");
-        config.append("certPrefix='' keyPrefix='' secmod='secmod.db' flags=readOnly\"");
-        config.append("\n");
-
-        return config.toString();
+        return new Firefox().getPKCS11Configuration();
     }
 
     @Test(expected = IllegalStateException.class)
@@ -49,24 +35,24 @@ public class PKCS11KeyStoreTest
     public void shouldGetAliasesFromAPKCS11KeyStore() throws FileNotFoundException,
             GeneralSecurityException, IOException
     {
-        String driverConfiguration = getDriverConfiguration();
+        byte[] driverConfiguration = getDriverConfiguration();
 
         SimpleKeyStore keyStore = new PKCS11KeyStore();
-        keyStore.load(new ByteArrayInputStream(driverConfiguration.getBytes()), "");
+        keyStore.load(new ByteArrayInputStream(driverConfiguration), "");
         List<String> aliases = keyStore.aliases();
 
         Assert.assertEquals(NUM_CERTIFICATES_IN_STORE, aliases.size());
         Assert.assertEquals(SIGN_CERTIFICATE_ALIAS, aliases.get(0));
     }
-    
+
     @Test
     public void shouldPKCS11KeyStoreGetAliasFromCertificate() throws FileNotFoundException,
             GeneralSecurityException, IOException
     {
-        String driverConfiguration = getDriverConfiguration();
-        
+        byte[] driverConfiguration = getDriverConfiguration();
+
         SimpleKeyStore keyStore = new PKCS11KeyStore();
-        keyStore.load(new ByteArrayInputStream(driverConfiguration.getBytes()), "");
+        keyStore.load(new ByteArrayInputStream(driverConfiguration), "");
 
         Certificate certificate = keyStore.getCertificate(SIGN_CERTIFICATE_ALIAS);
 
