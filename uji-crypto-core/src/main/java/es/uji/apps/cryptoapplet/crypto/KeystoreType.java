55eb0b3722666e5e70286dfb71d8bc13eb44123c
Ricardo Borillo <borillo@gmail.com>
2012-06-12 10:31:48 +0000

    Renamed supported keystores and reviewed values
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@8928 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/KeystoreType.java
@@ -0,0 +1,6 @@
+package es.uji.apps.cryptoapplet.crypto;
+
+public enum KeystoreType
+{
+    MSCAPI, PKCS11, PKCS12
+}
--- uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/SupportedKeystore.java
+++
@@ -1,6 +0,0 @@
-package es.uji.apps.cryptoapplet.crypto;
-
-public enum SupportedKeystore
-{
-    MSCAPI, MOZILLA, CLAUER, PKCS11, PKCS12
-}
