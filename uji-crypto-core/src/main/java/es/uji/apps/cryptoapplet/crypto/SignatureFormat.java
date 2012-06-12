f7792557e3a2fa7763327f3fed1e4d3bef7b8d7c
Ricardo Borillo <borillo@gmail.com>
2012-06-12 10:31:12 +0000

    Clarify signature formats supported
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@8927 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/SignatureFormat.java
+++ uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/SignatureFormat.java
@@ -2,49 +2,53 @@ package es.uji.apps.cryptoapplet.crypto;
 
 public enum SignatureFormat
 {
-    RAW {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.raw.RawSignatureFactory"; 
+    RAW
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.raw.RawFormatter";
         }
     },
-    CMS {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.cms.CMSSignatureFactory"; 
-        }
-    }, 
-    CMS_HASH {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.cms.CMSSignatureFactory"; 
-        }
-    }, 
-    XMLSIGNATURE {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.xmldsign.XMLDsigSignatureFactory"; 
-        }
-    }, 
-    XADES_JXADES {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.jxades.JXAdESSignatureFactory"; 
-        }
-    }, 
-    XADES_OPENXADES {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.openxades.OpenXAdESSignatureFactory"; 
-        }
-    }, 
-    PDF {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.pdf.PDFSignatureFactory"; 
-        }
-    }, 
-    ODF {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.xmldsign.odf.ODFSignatureFactory"; 
+    CMS
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.cms.CMSFormatter";
         }
     },
-    FACTURAE {
-        public String toString() {
-            return "es.uji.apps.cryptoapplet.crypto.facturae.FacturaeSignatureFactory"; 
+    XMLSIGNATURE
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.xmlsignature.XMLSignatureFormatter";
         }
-    }  
+    },
+    XADES
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.xades.XAdESFormatter";
+        }
+    },
+    PADES
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.pdf.PAdESFormatter";
+        }
+    },
+    ODF
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.xmlsignature.odf.ODFFormatter";
+        }
+    },
+    FACTURAE
+    {
+        public String toString()
+        {
+            return "es.uji.apps.cryptoapplet.crypto.xades.facturae.FacturaeFormatter";
+        }
+    }
 }
