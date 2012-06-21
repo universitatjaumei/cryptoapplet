d50728425e88df475dd852570a9ff9129ebed69f
Ricardo Borillo <borillo@gmail.com>
2012-06-21 13:05:16 +0000

    Añadida clase base para firma
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@9148 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-cms/src/main/java/es/uji/apps/cryptoapplet/crypto/cms/CMSFormatter.java
+++ uji-crypto-cms/src/main/java/es/uji/apps/cryptoapplet/crypto/cms/CMSFormatter.java
@@ -1,98 +1,84 @@
 package es.uji.apps.cryptoapplet.crypto.cms;
 
 import java.io.ByteArrayInputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.cert.CertStore;
 import java.security.cert.Certificate;
 import java.security.cert.CollectionCertStoreParameters;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bouncycastle.cms.CMSProcessableByteArray;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.CMSSignedGenerator;
 
-import es.uji.apps.cryptoapplet.config.i18n.LabelManager;
-import es.uji.apps.cryptoapplet.crypto.CryptoAppletCoreException;
+import es.uji.apps.cryptoapplet.crypto.BaseFormatter;
+import es.uji.apps.cryptoapplet.crypto.CertificateNotFoundException;
 import es.uji.apps.cryptoapplet.crypto.Formatter;
+import es.uji.apps.cryptoapplet.crypto.PrivateKeyNotFoundException;
+import es.uji.apps.cryptoapplet.crypto.SignatureException;
+import es.uji.apps.cryptoapplet.crypto.SignatureFormatException;
 import es.uji.apps.cryptoapplet.crypto.SignatureOptions;
 import es.uji.apps.cryptoapplet.crypto.SignatureResult;
 import es.uji.apps.cryptoapplet.utils.StreamUtils;
 import es.uji.security.crypto.cms.bc.MyCMSSignedDataGenerator;
 
-public class CMSFormatter implements Formatter
+public class CMSFormatter extends BaseFormatter implements Formatter
 {
-    @Override 
-    public SignatureResult format(SignatureOptions signatureOptions)
-            throws CryptoAppletCoreException
+    public CMSFormatter(X509Certificate certificate, PrivateKey privateKey, Provider provider)
+            throws PrivateKeyNotFoundException, CertificateNotFoundException
     {
+        super(certificate, privateKey, provider);
+    }
+
+    @Override
+    public SignatureResult format(SignatureOptions signatureOptions) throws SignatureException
+    {
+        checkSignatureOptions(signatureOptions);
+
         byte[] data = StreamUtils.inputStreamToByteArray(signatureOptions.getDataToSign());
-        X509Certificate certificate = signatureOptions.getCertificate();
-        PrivateKey privateKey = signatureOptions.getPrivateKey();
-        Provider provider = signatureOptions.getProvider();
-
-        MyCMSSignedDataGenerator gen = new MyCMSSignedDataGenerator();
-        SignatureResult signatureResult = new SignatureResult();
-
-        if (certificate == null)
-        {
-            signatureResult.setValid(false);
-            signatureResult.addError(LabelManager.get("ERROR_CMS_NOCERT"));
-
-            return signatureResult;
-        }
-
-        if (privateKey == null)
-        {
-            signatureResult.setValid(false);
-            signatureResult.addError(LabelManager.get("ERROR_CMS_NOKEY"));
-
-            return signatureResult;
-        }
-
-        gen.addSigner(privateKey, (X509Certificate) certificate, CMSSignedGenerator.DIGEST_SHA1);
-        CMSProcessableByteArray cmsProcessableByteArray = new CMSProcessableByteArray(data);
-
-        List<Certificate> certList = new ArrayList<Certificate>();
-
-        // TODO: Add the intermediate CAs if we have them
-        certList.add(certificate);
 
         try
         {
-            CertStore certst = CertStore.getInstance("Collection",
-                    new CollectionCertStoreParameters(certList));
-
-            gen.addCertificatesAndCRLs(certst);
+            MyCMSSignedDataGenerator formatter = new MyCMSSignedDataGenerator();
+            formatter.addSigner(privateKey, certificate, CMSSignedGenerator.DIGEST_SHA1);
+            formatter.addCertificatesAndCRLs(generateCertificateStore());
 
             if (signatureOptions.isHash())
             {
-                gen.setHash(data);
+                formatter.setHash(data);
             }
 
-            CMSSignedData cmsSignedData = gen.generate(cmsProcessableByteArray, provider);
+            CMSProcessableByteArray cmsProcessableByteArray = new CMSProcessableByteArray(data);
+            CMSSignedData cmsSignedData = formatter.generate(cmsProcessableByteArray, provider);
 
-            if (data != null)
+            if (data == null)
             {
-                signatureResult.setValid(true);
-                signatureResult.setSignatureData(new ByteArrayInputStream(cmsSignedData
-                        .getEncoded()));
-
-                return signatureResult;
+                throw new SignatureFormatException();
             }
-            else
-            {
-                signatureResult.setValid(false);
-                signatureResult.addError(LabelManager.get("ERROR_CMS_SIGNATURE"));
 
-                return signatureResult;
-            }
+            SignatureResult signatureResult = new SignatureResult();
+            signatureResult.setValid(true);
+            signatureResult.setSignatureData(new ByteArrayInputStream(cmsSignedData.getEncoded()));
+
+            return signatureResult;
         }
         catch (Exception e)
         {
-            throw new CryptoAppletCoreException(e);
+            throw new SignatureException(e);
         }
+    }
+
+    private CertStore generateCertificateStore() throws InvalidAlgorithmParameterException,
+            NoSuchAlgorithmException
+    {
+        List<Certificate> certList = Arrays.asList((Certificate) certificate);
+        CertStore certst = CertStore.getInstance("Collection", new CollectionCertStoreParameters(
+                certList));
+        return certst;
     }
 }
