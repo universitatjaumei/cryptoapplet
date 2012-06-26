e68cfdf8e62410642c789a5dece9fd33c201dd3c
Ricardo Borillo <borillo@gmail.com>
2012-06-26 11:31:45 +0000

    Added certificate chain to the BaseFormatter
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@9252 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/BaseFormatter.java
+++ uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/BaseFormatter.java
@@ -10,10 +10,18 @@ public class BaseFormatter
     protected final Provider provider;
     protected final PrivateKey privateKey;
     protected final X509Certificate certificate;
+    protected final X509Certificate[] caCertificates;
 
     public BaseFormatter(X509Certificate certificate, PrivateKey privateKey, Provider provider)
             throws PrivateKeyNotFoundException, CertificateNotFoundException,
             CertificateExpiredException
+    {
+        this(certificate, new X509Certificate[] {}, privateKey, provider);
+    }
+
+    public BaseFormatter(X509Certificate certificate, X509Certificate[] caCertificates,
+            PrivateKey privateKey, Provider provider) throws CertificateNotFoundException,
+            CertificateExpiredException, PrivateKeyNotFoundException
     {
         if (certificate == null)
         {
@@ -35,12 +43,13 @@ public class BaseFormatter
         }
 
         this.certificate = certificate;
+        this.caCertificates = caCertificates;
         this.privateKey = privateKey;
         this.provider = provider;
     }
 
     protected void checkSignatureOptions(SignatureOptions signatureOptions)
-            throws SignatureException
+            throws SignatureException, EmptyDocumentPassedToSignException
     {
         if (signatureOptions.getDataToSign() == null)
         {
