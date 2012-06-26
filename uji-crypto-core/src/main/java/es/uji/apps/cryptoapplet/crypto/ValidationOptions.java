0bc364782e6c3b1ec871555f16633dd9342cc585
Ricardo Borillo <borillo@gmail.com>
2012-06-26 11:31:09 +0000

    Change data interface to InputStream
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@9251 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/ValidationOptions.java
+++ uji-crypto-core/src/main/java/es/uji/apps/cryptoapplet/crypto/ValidationOptions.java
@@ -1,26 +1,28 @@
 package es.uji.apps.cryptoapplet.crypto;
 
+import java.io.InputStream;
+
 public class ValidationOptions
 {
-    private byte[] signedData;
-    private byte[] originalData;
+    private InputStream signedData;
+    private InputStream originalData;
 
-    public void setSignedData(byte[] signedData)
+    public void setSignedData(InputStream signedData)
     {
         this.signedData = signedData;
     }
 
-    public byte[] getSignedData()
+    public InputStream getSignedData()
     {
         return this.signedData;
     }
 
-    public void setOriginalData(byte[] originalData)
+    public void setOriginalData(InputStream originalData)
     {
         this.originalData = originalData;
     }
 
-    public byte[] getOriginalData()
+    public InputStream getOriginalData()
     {
         return this.originalData;
     }
