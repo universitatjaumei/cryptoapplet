93a8a47388b91a1ca05dd1bf8e7b914505cce01b
Ricardo Borillo <borillo@gmail.com>
2012-06-27 18:41:02 +0000

    Renamed browser class
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@9301 e6edf6fb-f266-4316-afb4-e53d95876a76

--- cryptoapplet-core/src/main/java/es/uji/apps/cryptoapplet/crypto/Browser.java
+++
@@ -1,6 +0,0 @@
-package es.uji.apps.cryptoapplet.crypto;
-
-public enum Browser
-{
-    IEXPLORER, FIREFOX, SAFARI
-}
---
+++ cryptoapplet-core/src/main/java/es/uji/apps/cryptoapplet/crypto/BrowserType.java
@@ -0,0 +1,6 @@
+package es.uji.apps.cryptoapplet.crypto;
+
+public enum BrowserType
+{
+    IEXPLORER, FIREFOX, CHROME, SAFARI, OPERA
+}
