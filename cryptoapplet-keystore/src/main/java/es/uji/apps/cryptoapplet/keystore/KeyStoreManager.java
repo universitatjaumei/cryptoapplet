ee7b273740f69ef7e176a04c9a892622652d133e
Ricardo Borillo <borillo@gmail.com>
2012-06-27 18:42:39 +0000

    Renamed browser class
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@9304 e6edf6fb-f266-4316-afb4-e53d95876a76

--- cryptoapplet-keystore/src/main/java/es/uji/apps/cryptoapplet/keystore/KeyStoreManager.java
+++ cryptoapplet-keystore/src/main/java/es/uji/apps/cryptoapplet/keystore/KeyStoreManager.java
@@ -4,34 +4,37 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
+import java.security.KeyStore.PrivateKeyEntry;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 import java.util.Hashtable;
+import java.util.Map.Entry;
 
-import es.uji.apps.cryptoapplet.crypto.Browser;
+import org.apache.log4j.Logger;
+
+import es.uji.apps.cryptoapplet.crypto.BrowserType;
 import es.uji.apps.cryptoapplet.crypto.KeystoreType;
 import es.uji.apps.cryptoapplet.keystore.mscapi.SunMSCAPIKeyStore;
 import es.uji.apps.cryptoapplet.keystore.pkcs11.PKCS11KeyStore;
 import es.uji.apps.cryptoapplet.keystore.pkcs11.devices.Firefox;
+import es.uji.apps.cryptoapplet.utils.OperatingSystemUtils;
 
 public class KeyStoreManager
 {
-    public Hashtable<KeystoreType, SimpleKeyStore> keystores;
-    private Browser navigator;
+    private Logger log = Logger.getLogger(KeyStoreManager.class);
 
-    public KeyStoreManager(Browser navigator)
+    public Hashtable<KeystoreType, SimpleKeyStore> keystores;
+    private BrowserType navigator;
+
+    public KeyStoreManager(BrowserType navigator)
     {
         this.navigator = navigator;
         this.keystores = new Hashtable<KeystoreType, SimpleKeyStore>();
     }
 
-    public void initBrowserStores() throws GeneralSecurityException, IOException
-    {
-        initInternetExplorerStore();
-        initFirefoxStore();
-    }
-
     private void initFirefoxStore() throws GeneralSecurityException, IOException
     {
-        if (navigator.equals(Browser.FIREFOX))
+        if (navigator.equals(BrowserType.FIREFOX))
         {
             Firefox firefox = new Firefox();
             InputStream pkcs11Configuration = new ByteArrayInputStream(
@@ -44,7 +47,9 @@ public class KeyStoreManager
 
     private void initInternetExplorerStore() throws GeneralSecurityException, IOException
     {
-        if (navigator.equals(Browser.IEXPLORER))
+        if (navigator.equals(BrowserType.IEXPLORER)
+                || (navigator.equals(BrowserType.CHROME) && OperatingSystemUtils
+                        .isWindowsUpperEqualToNT()))
         {
             SimpleKeyStore keystore = new SunMSCAPIKeyStore();
             keystore.load(null, null);
@@ -68,27 +73,58 @@ public class KeyStoreManager
 
     public void initKeyStores() throws GeneralSecurityException, IOException
     {
-        initBrowserStores();
+        initInternetExplorerStore();
+        initFirefoxStore();
         initPKCS11();
     }
 
     private void initPKCS11()
     {
-        //TODO
-//        if (!Browser.IEXPLORER.equals(navigator))
-//        {
-//            Device device = Device.getDeviceWithAvailableLibrary();
-//
-//            for (int deviceSlot = 0; deviceSlot < 4; deviceSlot++)
-//            {
-//                device.setSlot(deviceSlot);
-//                device.init();
-//            }
-//        }
+        // TODO
+        // if (!Browser.IEXPLORER.equals(navigator))
+        // {
+        // Device device = Device.getDeviceWithAvailableLibrary();
+        //
+        // for (int deviceSlot = 0; deviceSlot < 4; deviceSlot++)
+        // {
+        // device.setSlot(deviceSlot);
+        // device.init();
+        // }
+        // }
     }
 
     public void flushKeyStoresTable()
     {
         keystores.clear();
+    }
+
+    public PrivateKeyEntry getPrivateKeyEntryByDN(String certificateDN)
+    {
+        
+        try
+        {
+            for (Entry<KeystoreType, SimpleKeyStore> keystore : keystores.entrySet())
+            {
+                SimpleKeyStore simpleKeyStore = keystore.getValue();
+                
+                for (String alias : keystore.getValue().aliases())
+                {
+                    X509Certificate certificate = (X509Certificate) simpleKeyStore.getCertificate(alias);
+
+                    if (certificateDN
+                            .equalsIgnoreCase(certificate.getSubjectDN().toString()))
+                    {
+                        PrivateKey privateKey = (PrivateKey) simpleKeyStore.getKey(alias);
+                        return new PrivateKeyEntry(privateKey, new X509Certificate[] {certificate});
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            log.warn(e);
+        }
+
+        return null;
     }
 }
