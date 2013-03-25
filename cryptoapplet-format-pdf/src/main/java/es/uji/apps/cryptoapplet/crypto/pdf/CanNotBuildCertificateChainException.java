434d0cab7dc5d2ea14f348e4e85e87ba3feacf9d
Ricardo Borillo <borillo@gmail.com>
2013-03-25 13:50:07 +0000

    Additional crypto modules will be enabled in future RC
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@13828 e6edf6fb-f266-4316-afb4-e53d95876a76

--- cryptoapplet-format-pdf/src/main/java/es/uji/apps/cryptoapplet/crypto/pdf/CanNotBuildCertificateChainException.java
+++
@@ -1,8 +0,0 @@
-package es.uji.apps.cryptoapplet.crypto.pdf;
-
-import es.uji.apps.cryptoapplet.crypto.SignatureException;
-
-@SuppressWarnings("serial")
-public class CanNotBuildCertificateChainException extends SignatureException
-{
-}
