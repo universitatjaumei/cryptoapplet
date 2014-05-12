ec02fb5c51d7ce4ed0ce9f27cf3760edf5536b26
Ricardo Borillo <borillo@gmail.com>
2014-05-12 08:09:00 +0000

    Look for duplicated certs on the cert tree
    git-svn-id: http://jira.uji.es/repos/uji/CRYPTOAPPLET/trunk@21479 e6edf6fb-f266-4316-afb4-e53d95876a76

--- uji-ui-applet/src/main/java/es/uji/security/ui/applet/JTreeCertificateBuilder.java
+++ uji-ui-applet/src/main/java/es/uji/security/ui/applet/JTreeCertificateBuilder.java
@@ -2,6 +2,7 @@ package es.uji.security.ui.applet;
 
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
 
@@ -40,10 +41,11 @@ public class JTreeCertificateBuilder
         Vector<String> caStrs = new Vector<String>();
         Vector<DefaultMutableTreeNode> caNodes = new Vector<DefaultMutableTreeNode>();
 
-        for (IKeyStore keystore : ksh.values())
+        for (SupportedKeystore supportedKeystore : ksh.keySet())
         {
             try
             {                               
+            	IKeyStore keystore = ksh.get(supportedKeystore);
                 Certificate[] certs = keystore.getUserCertificates();
                 
                 if (certs != null)
@@ -52,22 +54,37 @@ public class JTreeCertificateBuilder
                     for (Certificate cer : certs)
                     {
                         found = false;
-                        xcert = (X509Certificate) cer;
-
+                        xcert = (X509Certificate) cer;                        
+                        
                         if (xcert != null)
                         {
                             certHandle = new X509CertificateHandler(xcert, "none", keystore);
-
                             for (int j = 0; j < caStrs.size(); j++)
                             {
                                 if (((String) caStrs.get(j)).equals(certHandle
                                         .getIssuerOrganization()))
                                 {
-                                    ((DefaultMutableTreeNode) caNodes.get(j))
-                                            .add(new DefaultMutableTreeNode(certHandle));
-                                    found = true;
-
-                                    log.debug("Added new certificate " + certHandle);
+                                	found = true;
+                                	
+                                	DefaultMutableTreeNode caNode = (DefaultMutableTreeNode) caNodes.get(j);
+                                	DefaultMutableTreeNode certHandleNode = new DefaultMutableTreeNode(certHandle);
+                                    int childIndex = certIndex(caNode, certHandleNode);
+                                    if (childIndex >= 0)
+                                    {
+                                    	if (supportedKeystore.equals(SupportedKeystore.PKCS11))
+                                    	{
+                                    		((DefaultMutableTreeNode) caNode.getChildAt(childIndex)).setUserObject(certHandleNode.getUserObject());
+                                    	
+                                    		log.debug("Replaced with PKCS11 certificate " + certHandle);
+                                    	}
+                                    }
+                                    else
+                                    {
+	                                    ((DefaultMutableTreeNode) caNodes.get(j))
+	                                            .add(new DefaultMutableTreeNode(certHandle));
+	                                    
+	                                    log.debug("Added new certificate " + certHandle);
+                                    }
                                 }
                             }
 
@@ -98,4 +115,24 @@ public class JTreeCertificateBuilder
         }
         return root;
     }
+
+	private int certIndex(DefaultMutableTreeNode caNode, DefaultMutableTreeNode certHandleNode) {
+		Enumeration children = caNode.children();
+		int i = 0;
+		
+		
+		while (children.hasMoreElements())
+		{
+			DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) children.nextElement();
+			X509CertificateHandler treeNodeHandle = (X509CertificateHandler) treeNode.getUserObject();
+			X509CertificateHandler certHandle = (X509CertificateHandler) certHandleNode.getUserObject();
+			if (treeNodeHandle.getCertificate().getSubjectDN().equals(certHandle.getCertificate().getSubjectDN()))
+			{
+				return i;
+			}
+			i++;
+		}
+		
+		return -1;
+	}
 }
