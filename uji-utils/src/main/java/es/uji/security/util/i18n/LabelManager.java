88cf344f281220eee0d2f603d2d0f3fa17004aae
Jaime Hablutzel <hablutzel1@gmail.com>
2015-05-25 14:34:56 -0500

    #2 Loading resource bundles in a standard fashion to avoid incompatibility with IcedTea-Web.

--- uji-utils/src/main/java/es/uji/security/util/i18n/LabelManager.java
+++ uji-utils/src/main/java/es/uji/security/util/i18n/LabelManager.java
@@ -1,6 +1,7 @@
 package es.uji.security.util.i18n;
 
 import java.util.Enumeration;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -24,11 +25,18 @@ public class LabelManager
 
             if (_lang != null)
             {
-                bundle = CustomBundleLoader.getBundle("i18n" + "_" + _lang);
+                /* supporting the input in the following non-standard format:
+                     uppercase_language "_" lowercase_country
+                   see http://jira.uji.es/confluence/display/CRYPTOAPPLET/Home#Home-Funcionesexportadasporelapplet */
+                String[] localeParts = _lang.split("_");
+                String language = localeParts[0].toLowerCase();
+                String country = localeParts[1].toUpperCase();
+
+                bundle = ResourceBundle.getBundle("i18n", new Locale(language, country));
             }
             else
             {
-                bundle = CustomBundleLoader.getBundle("i18n");
+                bundle = ResourceBundle.getBundle("i18n");
             }
 
             Enumeration<String> enume = bundle.getKeys();
